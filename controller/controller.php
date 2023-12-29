f484811a13d9201b156255d1d524a3ecaf442525
Shakib Ahmed <112560247+expertskb@users.noreply.github.com>
2023-12-29 11:25:47 +0600

    Add files via upload

--- controller/controller.php
+++ controller/controller.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Author : Shakib Ahmed
+E-mail : shakibwap@yahoo.com
+WEB: https://shakib.eu.org
+*/
 include_once('./controller/homecontroller.php');
 include_once('./controller/wishcontroller.php');
 include_once('./controller/notfoundcontroller.php');
